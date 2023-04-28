914a56d7dc2b0e912c724e2a62a5f046ed0b1d1a
leonid <leonidsleo@yandex.ru>
2023-04-28 16:13:42 +0300

    int на short в 19 задаче, т.к. строго указано, что 5 цифр, можно применитьт экономию места

--- C_sharp_sem3_dom19/Program.cs
+++ C_sharp_sem3_dom19/Program.cs
@@ -4,12 +4,46 @@
 // 12821 -> да
 // 23432 -> да
 
+// Console.WriteLine("Палиндром или нет число из пяти знаков?");
+// Console.WriteLine();
+// Console.Write("Введите пятизначное число n = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// int L = (int)Math.Log10(n)+1;
+// //Console.WriteLine(L);
+// if (L != 5)
+// {
+//     Console.WriteLine($"Число {n} не является пятизначным. Повторите ввод!");
+// }
+// else
+// {
+//     int a5 = n % 10;
+//     n = n / 10;
+//     int a4 = n % 10;
+//     n = n / 10;
+//     int a3 = n % 10;
+//     n = n / 10;
+//     int a2 = n % 10;
+//     n = n / 10;
+//     int a1 = n % 10;
+//     if (a1 == a5 || a2 == a4)
+//     {
+//         Console.WriteLine("Число n является палиндромом!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число n не является палиндромом!");
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Стоп");
+
 Console.WriteLine("Палиндром или нет число из пяти знаков?");
 Console.WriteLine();
 Console.Write("Введите пятизначное число n = ");
-int n = Convert.ToInt32(Console.ReadLine());
+short n = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine();
-int L = (int)Math.Log10(n)+1;
+short L = Convert.ToInt16((int)Math.Log10(n)+1);
 //Console.WriteLine(L);
 if (L != 5)
 {
@@ -17,15 +51,15 @@ if (L != 5)
 }
 else
 {
-    int a5 = n % 10;
-    n = n / 10;
-    int a4 = n % 10;
-    n = n / 10;
-    int a3 = n % 10;
-    n = n / 10;
-    int a2 = n % 10;
-    n = n / 10;
-    int a1 = n % 10;
+    short a5 = Convert.ToInt16(n % 10);
+    n = Convert.ToInt16(n / 10);
+    short a4 = Convert.ToInt16(n % 10);
+    n = Convert.ToInt16(n / 10);
+    short a3 = Convert.ToInt16(n % 10);
+    n = Convert.ToInt16(n / 10);
+    short a2 = Convert.ToInt16(n % 10);
+    n = Convert.ToInt16(n / 10);
+    short a1 = Convert.ToInt16(n % 10);
     if (a1 == a5 || a2 == a4)
     {
         Console.WriteLine("Число n является палиндромом!");
