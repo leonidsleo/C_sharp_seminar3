8b5b3e75e583a9a582bdfdb1289f717c7ec15175
leonid <leonidsleo@yandex.ru>
2023-04-28 16:17:20 +0300

    в задаче 19 поменял местами решения, первое через short

--- C_sharp_sem3_dom19/Program.cs
+++ C_sharp_sem3_dom19/Program.cs
@@ -4,40 +4,6 @@
 // 12821 -> да
 // 23432 -> да
 
-// Console.WriteLine("Палиндром или нет число из пяти знаков?");
-// Console.WriteLine();
-// Console.Write("Введите пятизначное число n = ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine();
-// int L = (int)Math.Log10(n)+1;
-// //Console.WriteLine(L);
-// if (L != 5)
-// {
-//     Console.WriteLine($"Число {n} не является пятизначным. Повторите ввод!");
-// }
-// else
-// {
-//     int a5 = n % 10;
-//     n = n / 10;
-//     int a4 = n % 10;
-//     n = n / 10;
-//     int a3 = n % 10;
-//     n = n / 10;
-//     int a2 = n % 10;
-//     n = n / 10;
-//     int a1 = n % 10;
-//     if (a1 == a5 || a2 == a4)
-//     {
-//         Console.WriteLine("Число n является палиндромом!");
-//     }
-//     else
-//     {
-//         Console.WriteLine("Число n не является палиндромом!");
-//     }
-// }
-// Console.WriteLine();
-// Console.WriteLine("Стоп");
-
 Console.WriteLine("Палиндром или нет число из пяти знаков?");
 Console.WriteLine();
 Console.Write("Введите пятизначное число n = ");
@@ -71,3 +37,38 @@ else
 }
 Console.WriteLine();
 Console.WriteLine("Стоп");
+
+
+// Console.WriteLine("Палиндром или нет число из пяти знаков?");
+// Console.WriteLine();
+// Console.Write("Введите пятизначное число n = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// int L = (int)Math.Log10(n)+1;
+// //Console.WriteLine(L);
+// if (L != 5)
+// {
+//     Console.WriteLine($"Число {n} не является пятизначным. Повторите ввод!");
+// }
+// else
+// {
+//     int a5 = n % 10;
+//     n = n / 10;
+//     int a4 = n % 10;
+//     n = n / 10;
+//     int a3 = n % 10;
+//     n = n / 10;
+//     int a2 = n % 10;
+//     n = n / 10;
+//     int a1 = n % 10;
+//     if (a1 == a5 || a2 == a4)
+//     {
+//         Console.WriteLine("Число n является палиндромом!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число n не является палиндромом!");
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Стоп");
